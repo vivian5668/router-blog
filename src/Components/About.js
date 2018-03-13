c45641cb8338ef155ee140f497d10e7be6b84f4d
Chelsea Zhu <qianwei5668@gmail.com>
2018-03-13 14:54:30 -0700

    adding Mongo after this commit

--- src/Components/About.js
+++ src/Components/About.js
@@ -1,14 +1,54 @@
 import React, { Component } from 'react';
+const axios = require('axios');
 
-const About = () => (
-	
-		<div>
-			<h1> About </h1>
-			<p>
-				This is my Blog!
-			</p>
+class About extends Component {
 
-		</div>
-)
+  constructor(props) {
+    super(props);
+    this.state = {
+      weather: '',
+      zipcode: ''
+  }
+
+    this.handleChange = this.handleChange.bind(this);
+    this.handleSubmit = this.handleSubmit.bind(this);
+  }
+
+  handleChange(event) {
+    this.setState({zipcode: event.target.value})
+    console.log('Your zip code is' + this.state.zipcode)
+  }
+
+  handleSubmit(event) {
+  	event.preventDefault()
+  	let weatherApi = 'http://api.openweathermap.org/data/2.5/weather?zip='+ this.state.zipcode + ',us&appid=052f26926ae9784c2d677ca7bc5dec98'
+    axios.get(weatherApi)
+    	.then((response) => {
+    		console.log(response.data.base)
+    		this.setState({weather: response.data.main.temp})
+    	}).catch((error) => {
+    		console.log('error occured: ', error)
+    	})
+  }
+
+  render() {
+    return (
+      <div>
+       
+      <form onSubmit={this.handleSubmit}>
+        <p>
+      		Temperature: {this.state.weather}
+        </p>
+        <label>
+          Please enter your zip code for the weather:
+          <input type="text" onChange={this.handleChange} />
+        </label>
+        <input type="submit" value="Get my forecast!" />
+      </form>
+      </div>
+    )
+  }
+}
+
 
 export default About
--- src/Components/Home.js
+++ src/Components/Home.js
@@ -1,14 +1,79 @@
 import React, { Component } from 'react';
+const axios = require('axios');
 
-const Home = () => (
-	
-		<div>
-			<h1> Home </h1>
-			<p>
-				Welcome to my home page!
-			</p>
+class Home extends Component{
+	constructor(props) {
+		super(props)
+		this.state = {
+			shakeSpeare: ''
+		}
+	}
 
-		</div>
-)
+	componentDidMount() {
+
+		//API url
+		let poemApi = 'http://ShakeItSpeare.com/api/poem'
+
+		//***USE FETCH OR AXIOS, PICK ONE
+
+		//fetch the API
+		//fetch is build-in in almost anything, no need to import
+		//return the response in json format
+
+		//***option 1
+		fetch(poemApi)
+			.then((response) => {
+				return response.json()
+			}).then((data) => {
+				this.setState({shakeSpeare: data.poem})
+			}).catch((error) => {
+				console.log('error occurred while parsing! ', error)
+			})
+
+		//***option 2
+		axios.get(poemApi)
+			.then((response) => {
+				this.setState({shakeSpeare: response.data.poem})
+			}).catch((error) => {
+				console.log('error occurred while parsing', error)
+			})
+
+		//***option 3
+		axios({
+				method: 'GET',
+				url: poemApi
+			}).then((response) => {
+				this.setState({shakeSpeare: response.data.poem})
+			})
+	}
+
+
+
+	render() {
+		let poetry = this.state.shakeSpeare
+		if (this.state.shakeSpeare) {
+			return (
+				<div>
+					<h1> Home </h1>
+					<h2> My fav Shakespeare </h2>
+					<p>
+						{poetry}
+					</p>
+
+				</div>
+			)} else {
+			return (
+				<div>
+					<h1> Home </h1>
+					<h2> My fav Shakespeare </h2>
+					<p>
+						Loading....
+					</p>
+
+				</div>
+			)}
+	}
+		
+}
 
 export default Home
